7619c67fcd671667b4df346c4d0639953a8f2822
luizpq <luizfabiano1007@gmail.com>
2024-08-23 10:54:53 -0300

    fazendo alguns exercícios do meu curso de javascript

--- exercícios/scripts/11-todo-list.js
+++ exercícios/scripts/11-todo-list.js
@@ -1,8 +1,33 @@
 const todoList = []
 const button = document.getElementById('button')
+renderTodoList()
+
+function renderTodoList() {
+    let todoListHTML = ''
+
+    for (let i = 0; i < todoList.length; i++) {
+        const todo = todoList[i]
+        const html = `
+        <p>
+        ${todo} 
+        <button onclick="
+        
+        todoList.splice(${i}, 1)
+        renderTodoList()
+
+        ">Delete</button>
+        </p>`
+        todoListHTML += html
 
 
+    }
 
+    console.log(todoListHTML)
+
+    document.querySelector('.js-todo-list')
+        .innerHTML = todoListHTML
+        
+}
 
 const addTodo = () => {
     const nameInput = document.querySelector('.js-name-input')
@@ -10,7 +35,20 @@ const addTodo = () => {
     
     todoList.push(name)
     console.log(todoList)
+
+    nameInput.value = ''
+
+    renderTodoList()
 }
+
+
+function removeTodo() {
+ 
+}
+
+
+const deleteButton = document.querySelector('.js-delete-button')
+
 
 
 button.addEventListener('click', () => {
